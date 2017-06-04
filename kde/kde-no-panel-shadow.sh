ba4822441cc528ab14a8341da47b0ee27133cebb
Emirald Mateli <aldo.mateli@gmail.com>
2017-06-04 20:10:21 +0200

    Script now waits for plasma to fully load

--- kde/kde-no-panel-shadow.sh
+++ kde/kde-no-panel-shadow.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-# remove shadow effect from plasma panels
-for WID in $(xwininfo -root -tree | sed '/"Plasma": ("plasmashell" "plasmashell")/!d; s/^  *\([^ ]*\) .*/\1/g'); do
-   xprop -id $WID -remove _KDE_NET_WM_SHADOW
+# Removes shadow from plasma panels
+
+while : 
+do
+    panels=($(xwininfo -root -tree | sed '/"Plasma": ("plasmashell" "plasmashell")/!d; s/^  *\([^ ]*\) .*/\1/g'))
+
+    # Need a more reliable way to find when plasma is fully loaded 
+    # rather than counting the panels  
+    if [ "${#panels[@]}" -ge 11 ]; then
+        for WID in "${panels[@]}"; do
+            xprop -id $WID -remove _KDE_NET_WM_SHADOW
+        done
+
+        exit 0
+    fi
+
+    sleep .5
 done
