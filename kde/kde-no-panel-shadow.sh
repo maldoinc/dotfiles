4a600520376cbe598a2c8fa49bb9d3a812209fd6
Emirald Mateli <aldo.mateli@gmail.com>
2017-06-04 22:43:27 +0200

    Updated panel find method. xprop is called only for the taskbar(s)

--- kde/kde-no-panel-shadow.sh
+++ kde/kde-no-panel-shadow.sh
@@ -4,11 +4,10 @@
 
 while : 
 do
-    panels=($(xwininfo -root -tree | sed '/"Plasma": ("plasmashell" "plasmashell")/!d; s/^  *\([^ ]*\) .*/\1/g'))
+    panels=($(xwininfo -root -tree | grep -P "\"Plasma\":.+plasmashell.+\d{4}x\d{2}" | awk '{ print $1 }'))
 
-    # Need a more reliable way to find when plasma is fully loaded 
-    # rather than counting the panels  
-    if [ "${#panels[@]}" -ge 11 ]; then
+    if [ "${#panels[@]}" -gt 0 ]; then
+        sleep 5
         for WID in "${panels[@]}"; do
             xprop -id $WID -remove _KDE_NET_WM_SHADOW
         done
