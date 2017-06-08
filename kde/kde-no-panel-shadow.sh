bcaba7792578d77bc0765e6e67e23ce8df8b92bb
Emirald Mateli <aldo.mateli@gmail.com>
2017-06-08 13:40:51 +0200

    Removed sleep call

--- kde/kde-no-panel-shadow.sh
+++ kde/kde-no-panel-shadow.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Removes shadow from plasma panels
-
 while : 
 do
     panels=($(xwininfo -root -tree | grep -P "\"Plasma\":.+plasmashell.+\d{4}x\d{2}" | awk '{ print $1 }'))
 
     if [ "${#panels[@]}" -gt 0 ]; then
-        sleep 5
         for WID in "${panels[@]}"; do
             xprop -id $WID -remove _KDE_NET_WM_SHADOW
         done
