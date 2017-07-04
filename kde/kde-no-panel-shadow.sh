8b704dfe8d97bf9444a9538e1100cf6bbe7e897f
Emirald Mateli <aldo.mateli@gmail.com>
2017-07-04 22:39:12 +0200

    Proper panel detection regardless of resolution

--- kde/kde-no-panel-shadow.sh
+++ kde/kde-no-panel-shadow.sh
@@ -2,7 +2,7 @@
 
 while : 
 do
-    panels=($(xwininfo -root -tree | grep -P "\"Plasma\":.+plasmashell.+\d{4}x\d{2}" | awk '{ print $1 }'))
+    panels=($(xwininfo -root -tree | grep -P "\s{6}.+\"Plasma\":.+plasmashell.+" | awk '{ print $1 }'))
 
     if [ "${#panels[@]}" -gt 0 ]; then
         for WID in "${panels[@]}"; do
