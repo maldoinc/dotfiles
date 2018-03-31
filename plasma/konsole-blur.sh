14e13c3bc6d7a4c32ac1b1754444f2732eedf8bc
Emirald Mateli <1253062+maldoinc@users.noreply.github.com>
2018-03-31 19:39:37 +0200

    Update to work with yakuake

--- plasma/konsole-blur.sh
+++ plasma/konsole-blur.sh
@@ -1,5 +1,8 @@
 if [ `echo $DISPLAY` ]; then
-    while read -r line; do
-        xprop -f _KDE_NET_WM_BLUR_BEHIND_REGION 32c -set _KDE_NET_WM_BLUR_BEHIND_REGION 0 -id `qdbus $line /konsole/MainWindow_1 winId`
-    done <<< $(qdbus | grep konsole | cut -f 2 -d\ )
+    konsole=`qdbus | grep konsole | cut -f 2 -d\\`
+    if [[ -n $konsole ]]; then
+        while read -r line; do
+            xprop -f _KDE_NET_WM_BLUR_BEHIND_REGION 32c -set _KDE_NET_WM_BLUR_BEHIND_REGION 0 -id `qdbus $line /konsole/MainWindow_1 winId`
+        done <<< $konsole;
+    fi
 fi
